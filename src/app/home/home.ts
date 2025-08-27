ff8b6f413579a3e675d7295d216f3fa4fbb439ee
月と猫 - LunaNeko <lunaticlegacy@163.com>
2025-08-27 18:20:14 +0800

    Add all files and initialize my Angular project.

---
+++ src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+
+export class Home {
+  people_view = 0;
+}
